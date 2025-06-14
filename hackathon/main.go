717247192775c2bc4acab079ce7dbb33cea6fd77
LIONS19 <shuntaro.t1022@gmail.com>
2025-06-14 16:28:20 +0900

    update main.go

--- hackathon/main.go
+++ hackathon/main.go
@@ -3,27 +3,49 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func main() {
-	mysqlUser := os.Getenv("MYSQL_USER")
-	mysqlPwd := os.Getenv("MYSQL_PWD")
-	mysqlHost := os.Getenv("MYSQL_HOST")
-	mysqlDatabase := os.Getenv("MYSQL_DATABASE")
+func userHandler(w http.ResponseWriter, r *http.Request) {
+	// 環境変数から DB 情報取得
+	user := os.Getenv("MYSQL_USER")
+	pwd := os.Getenv("MYSQL_PWD")
+	host := os.Getenv("MYSQL_HOST")
+	dbname := os.Getenv("MYSQL_DATABASE")
 
-	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s", mysqlUser, mysqlPwd, mysqlHost, mysqlDatabase)
+	// DSNの組み立て
+	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s", user, pwd, host, dbname)
 	db, err := sql.Open("mysql", dsn)
 	if err != nil {
-		panic(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 	defer db.Close()
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Connected to DB!")
-	})
-	http.ListenAndServe(":8080", nil)
+	rows, err := db.Query("SELECT id, name, age FROM user")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var id, name string
+		var age int
+		if err := rows.Scan(&id, &name, &age); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		fmt.Fprintf(w, "id: %s, name: %s, age: %d\n", id, name, age)
+	}
+}
+
+func main() {
+	http.HandleFunc("/users", userHandler)
+	log.Println("Listening on port 8080...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
