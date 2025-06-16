b7b244d138ac93f228479c5ffb7ed94fe70cb3ad
LIONS19 <shuntaro.t1022@gmail.com>
2025-06-17 04:12:41 +0900

    fix cors

--- hackathon/middleware/cors.go
+++ hackathon/middleware/cors.go
@@ -4,7 +4,19 @@ import "net/http"
 
 func WithCORS(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		origin := r.Header.Get("Origin")
+        // 許可するオリジンを動的に検証して追加
+        allowedOrigins := []string{
+            "http://localhost:3000",
+            "https://hackathon-frontend-wheat-tau.vercel.app", // ← Vercel の本番ドメイン
+        }
+
+        for _, o := range allowedOrigins {
+            if o == origin {
+                w.Header().Set("Access-Control-Allow-Origin", o)
+                break
+            }
+        }
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
